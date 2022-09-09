3b1e4cfd6234036ba407c12ea806261945d88734
Akpolo Ogagaoghene Prince <ogazboizakpolo@gmail.com>
2022-09-09 02:20:20 -0700

    done with the 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char qu[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+char qu[] ="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, qu, sizeof(qu));
 return (1);
 }
