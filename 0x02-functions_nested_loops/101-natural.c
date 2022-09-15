e5231db7c30704cacf4efc6d35fb2f24667a9caf
Akpolo Ogagaoghene Prince <ogazboizakpolo@gmail.com>
2022-09-15 00:35:21 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,15 +7,16 @@ int main(void)
 {
 	unsigned long int m;
 	int i;
+
 	m = 0;
 
 	for (i = 0; i < 1024; ++i)
 	{
 		if (((i % 3) == 0) || ((i % 5) == 0))
 		{
-		       m = m + i;
+			m = m + i;
 		}
 	}
 	printf("%ld\n", m);
 	return (0);
-}	
+}
